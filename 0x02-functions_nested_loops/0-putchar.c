1c5f0ec5e1a4392c592e4f4bac7552a14997300c
redietttt <rtsdbe5@gmail.com>
2022-03-16 00:56:20 +0300

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char t[] = "_putchar";
+	int h;
+
+	for (h = 0; h < 8; ++h)
+		_putchar(t[h]);
+	_putchar('\n');
+
+	return (0);
+}
